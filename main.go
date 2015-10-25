082dc12adfdd5fd3188b6d55a2d6f7b819e75b1a
dongxu <huang@wandoujia.com>
2015-10-25 09:41:07 +0800

    bench: first commit
    1. concurrent insert testing data
    2. modify .gitignore for ignoring vim swap file

---
+++ main.go
@@ -0,0 +1,138 @@
+// Copyright 2015 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"bytes"
+	"database/sql"
+	"flag"
+	"fmt"
+	"strings"
+	"sync"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/ngaut/log"
+	"github.com/ngaut/pool"
+)
+
+var (
+	rows       = flag.Int("rows", 10000, "row number of bench table, default: 10000")
+	N          = flag.Int("N", 10000, "select/update times, default: 10000")
+	concurrent = flag.Int("c", 50, "concurrent workers, default: 50")
+	bulkSize   = flag.Int("bulk", 20, "test data size (one field, in byte), default: 20")
+	nCols      = flag.Int("cols", 2, "bench table column number, default: 2")
+	tblPrefix  = flag.String("prefix", "tidb", "bench table prefix, default: tidb")
+	addr       = flag.String("addr", ":4000", "tidb-server addr, default: :4000")
+	cases      = flag.String("t", "select", "test cases to run (select, update, delete), default: select,update")
+	dbName     = flag.String("db", "test", "db name, default: test")
+	force      = flag.Bool("f", true, "drop table first")
+	user       = flag.String("u", "root", "username, default: root")
+	password   = flag.String("p", "", "password, default: empty")
+	logLevel   = flag.String("L", "error", "log level, default: error")
+)
+
+const (
+	ConnPoolSize = 100
+)
+
+var (
+	connPool = pool.NewCache("pool", ConnPoolSize, func() interface{} {
+		db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s", *user, *password, *addr, *dbName))
+		if err != nil {
+			log.Fatal(err)
+		}
+		return db
+	})
+)
+
+func exec(sqlStmt string) error {
+	db := connPool.Get().(*sql.DB)
+	defer connPool.Put(db)
+	log.Debug("exec sql:", sqlStmt)
+	_, err := db.Exec(sqlStmt)
+	return err
+}
+
+func mustExec(sqlStmt string) {
+	if err := exec(sqlStmt); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func insertTestData(rows int, workers int) error {
+	log.Debug("start insert test data...")
+	idChan := make(chan int)
+	wg := sync.WaitGroup{}
+	for i := 0; i < workers; i++ {
+		// worker func
+		go func(workerId int) {
+			wg.Add(1)
+			defer wg.Done()
+			for {
+				id, ok := <-idChan
+				// all data sent
+				if !ok {
+					return
+				}
+				var bulks []string
+				for i := 0; i < *nCols; i++ {
+					// fill dummy data
+					buf := bytes.Repeat([]byte{'A'}, *bulkSize)
+					bulks = append(bulks, fmt.Sprintf("\"%s\"", string(buf)))
+				}
+				sql := fmt.Sprintf("INSERT INTO %s_bench VALUES(%d, %s);", *tblPrefix, id, strings.Join(bulks, ","))
+				mustExec(sql)
+			}
+		}(i)
+	}
+	for i := 0; i < rows; i++ {
+		idChan <- i
+	}
+	close(idChan)
+	wg.Wait()
+	log.Debug("insert test data successfully")
+	return nil
+}
+
+func dropTable() {
+	dropSql := fmt.Sprintf("DROP TABLE IF EXISTS %s_bench", *tblPrefix)
+	mustExec(dropSql)
+}
+
+func createTable() {
+	var fieldNames []string
+	for i := 0; i < *nCols; i++ {
+		fieldNames = append(fieldNames, fmt.Sprintf("f_%d TEXT", i))
+	}
+	fields := strings.Join(fieldNames, ",")
+	tblName := fmt.Sprintf("%s_bench", *tblPrefix)
+	if *force {
+		fmt.Println("drop bench table")
+		dropTable()
+	}
+	sql := fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s(id INT, %s, PRIMARY KEY(id))", tblName, fields)
+	mustExec(sql)
+	fmt.Println("create bench table successfully")
+}
+
+func main() {
+	flag.Parse()
+	log.SetLevelByString(*logLevel)
+	createTable()
+	c := time.Now()
+	fmt.Println("start insert test data")
+	insertTestData(*rows, *concurrent)
+	fmt.Println("insert test data OK, elapse:", time.Since(c))
+}
