37d5fce65bee01dd56f62bd228d161953627c065
zhexuany <yangzhexuan@pingcap.com>
2017-07-12 20:46:47 +0800

    update makefile

--- tpch/gen_load_script.sh
+++ tpch/gen_load_script.sh
@@ -6,15 +6,16 @@ write_to_file()
     rm -f loaddata.sql
     file="loaddata.sql"
 
+    DIR=`pwd`
     for tbl in `ls dbgen/*.tbl`; do
         table=$(echo "${tbl%.*}" | tr '[:lower:]' '[:upper:]')
-	      name=${table#$prefix}
-	      file="$name.sql"
-	      if [ ! -f "$file" ] ; then
-            touch "$file"
+	    name=${table#$prefix}
+	    file="$name.sql"
+	    if [ ! -f "$file" ] ; then
+            	touch "$file"
+	    fi
             echo "LOAD DATA LOCAL INFILE '$DIR/$tbl' INTO TABLE ${table#$prefix}" >> $file
             echo "FIELDS TERMINATED BY '|';" >> $file
-    	  fi
     done
 }
 
