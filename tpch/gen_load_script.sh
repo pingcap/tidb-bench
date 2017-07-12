cfb0096c36bd2d371b728248ec5f002b4ca78668
zhexuany <yangzhexuan@pingcap.com>
2017-07-12 14:10:44 +0800

    make load in concurrency mode

--- tpch/gen_load_script.sh
+++ tpch/gen_load_script.sh
@@ -6,16 +6,15 @@ write_to_file()
     rm -f loaddata.sql
     file="loaddata.sql"
 
-    if [ ! -f "$file" ] ; then
-        touch "$file"
-    fi
-
-    echo 'USE tpch;\n' >> $file
-    DIR=`pwd`
     for tbl in `ls dbgen/*.tbl`; do
         table=$(echo "${tbl%.*}" | tr '[:lower:]' '[:upper:]')
-        echo "LOAD DATA LOCAL INFILE '$DIR/$tbl' INTO TABLE ${table#$prefix}" >> $file
-        echo "FIELDS TERMINATED BY '|';" >> $file
+	      name=${table#$prefix}
+	      file="$name.sql"
+	      if [ ! -f "$file" ] ; then
+            touch "$file"
+            echo "LOAD DATA LOCAL INFILE '$DIR/$tbl' INTO TABLE ${table#$prefix}" >> $file
+            echo "FIELDS TERMINATED BY '|';" >> $file
+    	  fi
     done
 }
 
