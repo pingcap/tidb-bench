849bddcb7725c8abfba652186714215cbadfc5bd
CWen <cwenyin0@gmail.com>
2017-08-14 13:09:37 +0800

    sysbench: create database if the database is not exist (#16)

--- sysbench/prepare.sh
+++ sysbench/prepare.sh
@@ -4,6 +4,13 @@ set -x
 
 . ./conf.sh
 
+if [[ ${password} = "" ]];
+then
+    mysql -h ${host} -P ${port} -u${user} -e "CREATE DATABASE IF NOT EXISTS ${dbname}"
+else
+    mysql -h ${host} -P ${port} -u${user} -p{$password} -e "CREATE DATABASE IF NOT EXISTS ${dbname}"
+fi
+
 sysbench --test=./lua-tests/db/oltp.lua --db-driver=${driver} --mysql-host=${host} --mysql-port=${port} \
  --mysql-user=${user} --mysql-password=${password} --mysql-db=${dbname} \
  --oltp-tables-count=${tcount} --oltp-table-size=${tsize} --rand-init=on prepare
