0930eb003bd45e0d7ea4974648f958a9470651e7
cwen0 <cwenyin0@gmail.com>
2017-09-24 21:26:43 +0800

    update select lua

--- sysbench/select.sh
+++ sysbench/select.sh
@@ -4,7 +4,7 @@ set -x
 
 . ./conf.sh
 
-sysbench --test=./lua/select_random_points.lua --db-driver=${driver} --mysql-host=${host} --mysql-port=${port} \
+sysbench ./lua/select_random_points.lua --db-driver=${driver} --mysql-host=${host} --mysql-port=${port} \
  --mysql-user=${user} --mysql-password=${password} --mysql-db=${dbname} \
  --tables=${tcount} --table-size=${tsize} \
  --threads=${threads} --report-interval=${interval} \
