2eba1bf9fa4375ac214d48cf7f83cfbb8c01b3b1
Jian Zhang <zjsariel@gmail.com>
2018-03-11 22:22:11 +0800

    address comment

--- tpcds/load_data.sh
+++ tpcds/load_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu
 
-HOST="172.16.10.16"
+HOST="127.0.0.1"
 PORT="3306"
 USER="root"
 PASSWD="12345678"
@@ -36,7 +36,7 @@ load_data()
     touch $LOG_FILE
     for file_name in `ls tools/*.dat`; do
         table_file=$(echo "${file_name##*/}")
-        table_name=$(echo "${table_file%.*}" | tr '[:lower:]' '[:upper:]')
+        table_name=$(echo "${table_file%.*}" | tr '[:upper:]' '[:lower:]')
         load_data_sql="LOAD DATA LOCAL INFILE '$file_name' INTO TABLE $table_name FIELDS TERMINATED BY '|' LINES TERMINATED BY '\n';"
         mysql -h $HOST -P $PORT -u $USER --local-infile=1 -p$PASSWD -D $DATABASE -e "$load_data_sql" >> $LOG_FILE 2>&1 &
     done
