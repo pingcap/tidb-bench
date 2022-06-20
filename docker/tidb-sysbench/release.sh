82c8ef8dcb88ac30b187392bf1bf3b1cd9a112f3
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-20 22:06:57 +0800

    Delete docker/tidb-sysbench directory

--- docker/tidb-sysbench/release.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-TAG="${1:-"latest"}"
-docker tag "pingcap/tidb-sysbench:$TAG" "gcr.io/pingcap-public/pingcap/tidb-sysbench:$TAG"
-gcloud docker -- push "gcr.io/pingcap-public/pingcap/tidb-sysbench:$TAG"
